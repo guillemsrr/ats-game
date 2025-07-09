decdf2a202639e96f2e43b8b4ef980be466a9e1b
Guillem Serra <guillemserralorenz@gmail.com>
2025-07-09 08:35:51 +0200

    character player actions, basic scanner

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Character
+namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
@@ -11,17 +11,19 @@ namespace Character
         private InputAction _sprintAction;
         private InputAction _clickAction;
 
-        Camera _camera;
-
         [SerializeField] private float _movementSpeed = 20f;
         [SerializeField] private float _acceleration = 1.3f;
         [SerializeField] private float _smoothTime = 0.1f;
 
         private float _currentAcceleration = 1f;
-        private Vector3 _currentVelocity;
-        private Vector3 _smoothVelocity;
-
         private Vector2 _movementInput;
+
+        [SerializeField] private Vector3 _ellipseCenter = Vector3.zero;
+        [SerializeField] private float _radiusA = 10f;
+        [SerializeField] private float _radiusB = 5f;
+        [SerializeField] private float _rotationSpeed = 1f;
+
+        private float _angle = 0f;
 
         private void OnEnable()
         {
@@ -41,8 +43,6 @@ namespace Character
 
             _sprintAction.performed += OnSprintPressed;
             _sprintAction.canceled += OnSprintReleased;
-
-            _camera = Camera.main;
         }
 
         private void OnSprintReleased(InputAction.CallbackContext obj)
@@ -57,39 +57,23 @@ namespace Character
 
         private void Update()
         {
+            //TODO: reuse input?
             _movementInput = _moveAction.ReadValue<Vector2>();
         }
 
         private void FixedUpdate()
         {
-            Walk();
-            FaceDirection();
-        }
+            // Increment the angle over time to rotate
+            _angle += _rotationSpeed* _currentAcceleration * Time.fixedDeltaTime;
 
-        private void Walk()
-        {
-            Vector3 cameraForward = _camera.transform.forward;
-            Vector3 cameraRight = _camera.transform.right;
+            // Calculate the new position on the ellipse
+            float x = _ellipseCenter.x + _radiusA * Mathf.Cos(_angle);
+            float z = _ellipseCenter.z + _radiusB * Mathf.Sin(_angle);
+            Vector3 newPosition = new Vector3(x, transform.position.y, z);
 
-            cameraForward.y = 0;
-            cameraRight.y = 0;
-            cameraForward.Normalize();
-            cameraRight.Normalize();
-
-            Vector3 targetVelocity = (cameraRight * _movementInput.x + cameraForward * _movementInput.y) *
-                                     _movementSpeed * _currentAcceleration;
-            _currentVelocity = Vector3.SmoothDamp(_currentVelocity, targetVelocity, ref _smoothVelocity, _smoothTime);
-
-            transform.Translate(_currentVelocity * Time.fixedDeltaTime, Space.World);
-        }
-
-        private void FaceDirection()
-        {
-            if (_movementInput != Vector2.zero)
-            {
-                float angle = Mathf.Atan2(_movementInput.x, _movementInput.y) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.Euler(0, angle, 0);
-            }
+            // Update the position and look at the center
+            transform.position = newPosition;
+            transform.LookAt(_ellipseCenter);
         }
     }
 }
---
+++ Assets/Player/ScanManager.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Guillem Serra. All Rights Reserved.
+
+using System;
+using FogOfWar;
+using UnityEngine;
+
+namespace Player
+{
+    public class ScanManager : MonoBehaviour
+    {
+        [SerializeField] private RevealTransparencyHandler _revealTransparencyHandler;
+        [SerializeField] private Scanner _scanner;
+
+        [SerializeField] private float _scanLength = 5f;
+        [SerializeField] private float _scanOffset = 1f;
+
+        private bool _linearScan = true;
+
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        public void Activate()
+        {
+            _scanner.ScanStart += OnScanStart;
+            _scanner.ScanEnd += OnScanEnd;
+        }
+
+        public void DeActivate()
+        {
+            _scanner.ScanStart -= OnScanStart;
+            _scanner.ScanEnd -= OnScanEnd;
+        }
+
+        public void SetScanType(bool isLinear)
+        {
+            _linearScan = isLinear;
+        }
+
+        private void OnScanEnd()
+        {
+        }
+
+        private void OnScanStart()
+        {
+            if (_linearScan)
+            {
+                Vector3 startPosition = _scanner.transform.position;
+                Vector3 position = startPosition;
+                float distance = 0;
+                while (distance < _scanLength)
+                {
+                    _revealTransparencyHandler.RevealAtPoint(position);
+                    position += _scanner.transform.forward * _scanOffset;
+                    distance = Vector3.Distance(startPosition, position);
+                }
+            }
+            else
+            {
+                _revealTransparencyHandler.RevealAtPoint(_scanner.transform.position);
+            }
+        }
+    }
+}
